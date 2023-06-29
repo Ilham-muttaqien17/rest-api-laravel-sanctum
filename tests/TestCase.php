799d8860f83376bebb4d190e25637619b73256e1
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-06-29 22:07:43 +0700

    Initial custom log before run the test
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,18 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Log;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseTransactions;
+
+    protected $customLog;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->customLog = Log::channel('custom');
+    }
 }
