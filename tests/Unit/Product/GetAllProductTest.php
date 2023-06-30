ea77de9fdbbdb2d2098416d6d1975ef788cd690c
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-06-30 13:30:49 +0700

    Add test scenario for get all products
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

---
+++ tests/Unit/Product/GetAllProductTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Product;
+
+use Tests\TestCase;
+
+class GetAllProductTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_it_should_can_get_all_products(): void
+    {
+        $response = $this->json('GET', '/api/v1/products', [], [
+            "Accept" => "application/json",
+            "Content-Type" => "application/json"
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                "message",
+                "data"
+            ]);
+    }
+}
