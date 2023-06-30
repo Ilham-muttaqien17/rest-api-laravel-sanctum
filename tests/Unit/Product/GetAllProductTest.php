2cc867b5995e7c256c4da2b28d2fabcbdf2b2322
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-06-30 14:02:56 +0700

    Update test scenario for get all products
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- tests/Unit/Product/GetAllProductTest.php
+++ tests/Unit/Product/GetAllProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Product;
 
+use App\Models\Product;
 use Tests\TestCase;
 
 class GetAllProductTest extends TestCase
@@ -11,15 +12,20 @@ class GetAllProductTest extends TestCase
      */
     public function test_it_should_can_get_all_products(): void
     {
+        Product::factory()->count(3)->create();
+
         $response = $this->json('GET', '/api/v1/products', [], [
             "Accept" => "application/json",
             "Content-Type" => "application/json"
         ]);
 
+        $this->customLog->info("get all products", ["response" => $response]);
+
         $response->assertStatus(200)
             ->assertJsonStructure([
                 "message",
                 "data"
-            ]);
+            ])
+            ->assertJsonIsArray("data");
     }
 }
