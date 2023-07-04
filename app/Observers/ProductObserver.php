faeefb832e9f414912a02fbf15c0d436dc1e7b2d
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-07-04 16:25:28 +0700

    Update product observer for clearing cache
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Product;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
 
 class ProductObserver
 {
@@ -12,7 +13,20 @@ class ProductObserver
      */
     public function created(Product $product): void
     {
-        Cache::forget('products_list');
+        // Cache::forget('products_list');
+        $cachePath = storage_path('framework/cache/data');
+        $cacheFiles = File::files($cachePath);
+        $cacheKeys = [];
+
+        foreach ($cacheFiles as $file) {
+            $cacheKeys[] = pathinfo($file->getFilename(), PATHINFO_FILENAME);
+        }
+
+        foreach ($cacheKeys as $key) {
+            if (preg_match('/product_list:/i', $key)) {
+                Cache::forget($key);
+            }
+        }
     }
 
     /**
@@ -20,6 +34,19 @@ class ProductObserver
      */
     public function updated(Product $product): void
     {
-        Cache::forget('products_list');
+        // Cache::forget('products_list');
+        $cachePath = storage_path('framework/cache/data');
+        $cacheFiles = File::files($cachePath);
+        $cacheKeys = [];
+
+        foreach ($cacheFiles as $file) {
+            $cacheKeys[] = pathinfo($file->getFilename(), PATHINFO_FILENAME);
+        }
+
+        foreach ($cacheKeys as $key) {
+            if (preg_match('/product_list:/i', $key)) {
+                Cache::forget($key);
+            }
+        }
     }
 }
